f5ad7e8eb5da22719b7efc5db2774db555688e5e
pylover <vahid.mardani@gmail.com>
2019-10-02 00:58:39 +0330

    main

--- rootgit/__init__.py
+++ rootgit/__init__.py
@@ -1,3 +1,22 @@
+from easycli import Root
+
 
 __version__ = '0.1.0'
+
+
+class RootGit(Root):
+    __completion__ = True
+    __arguments__ = [
+    ]
+
+    def __call__(self, args):
+        if args.version:
+            print(__version__)
+            return
+
+        super().__call__(args)
+
+
+def main():
+    RootGit().main()
 
